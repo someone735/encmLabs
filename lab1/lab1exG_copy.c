8f70c83b773123eb619f8edce0782a6f3f9513b1
someone735 <smeone83@gmail.com>
2024-01-16 20:13:53 -0700

    update

--- lab1/lab1exG_copy.c
+++ lab1/lab1exG_copy.c
@@ -28,14 +28,17 @@ int main(void)
     printf("This program demonstrates use of Newton's Method to find\n"
            "approximate roots of the polynomial\nf(x) = ");
     printf("%.2f", f[0]);
-    for (i = 1; i <= POLY_DEGREE; i++)
-        if (f[i] < 0) goto anti_case;
-            printf(" + %.2f*pow(x,%d)", f[i], i);
-            goto end_if_l36;
-        anti_case:
+    i = 1;
+    goto forLoop1;
+    forLoop1:
+        if (f[i] < 0) goto antiCaseL35;
+        printf(" + %.2f*pow(x,%d)", f[i], i);
+        goto endIfL37;
+        antiCaseL35:
             printf(" - %.2f*pow(x,%d)", -f[i], i);
-        end_if_l36:
-        ;
+        endIfL37:
+        i++;
+        if (i <= POLY_DEGREE) goto forLoop1; 
 
     printf("\nPlease enter a guess at a root, and a maximum number of\n"
            "updates to do, separated by a space.\n");
@@ -44,6 +47,8 @@ int main(void)
         printf("Sorry, I couldn't understand the input.\n");
         exit(1);
     }
+
+    
   
     if (max_updates < 1)  {
         printf("Sorry, I must be allowed do at least one update.\n");
@@ -51,32 +56,42 @@ int main(void)
     }
     printf("Running with initial guess %f.\n", guess);
 
-    for (i = POLY_DEGREE - 1; i >= 0; i--)
+    i = POLY_DEGREE;
+    goto forLoop2;
+    forLoop2:
         dfdx[i] = (i + 1) * f[i + 1];   // Calculus!
+        i--;
+        if (i>=0) goto forLoop2;
+        
     current_x = guess;
     update_count = 0;
     flag = 0;
-    while (flag == 0) { 
+    whileLoop1:
         current_f = polyval(f, POLY_DEGREE, current_x);
         printf("%d update(s) done; x is %.15f; f(x) is %.15e\n",
                update_count, current_x, current_f);
-        if (fabs(current_f) < MAX_ABS_F)
+        if (fabs(current_f) >= MAX_ABS_F) goto checkUpdate;
             flag = 1;
-        else if (update_count == max_updates)
+            goto endIfL89;
+        checkUpdate:
+            if (update_count == max_updates)
             flag = 2;
+        endIfL89:
         if (!flag) {
           current_dfdx = polyval(dfdx, POLY_DEGREE - 1, current_x);
           current_x -= current_f / current_dfdx;
           update_count++;
         }
-    }
+        if (flag != 0) goto whileLoop1;
 
-    if (flag == 2)
-        printf("%d updates performed, |f(x)| still >= %g.\n", 
+    if (flag != 2) goto anticaseL101;
+    printf("%d updates performed, |f(x)| still >= %g.\n", 
                update_count, MAX_ABS_F);
-    else
+    goto endIfL105;    
+    anticaseL101:
         printf("Stopped with approximate solution of %.10f.\n", 
                current_x);
+    endIfL105:
     return 0;
 }
 
@@ -84,9 +99,12 @@ double polyval(const double *a, int n, double x)
 {
     double result = a[n];
     int i;
-    for (i = n - 1; i >= 0; i--) {
+    i = n-1;
+    goto forLoop3;
+    forLoop3:
         result *= x;
         result += a[i];
-    }
+        i--;
+        if (i >= 0) goto forLoop3;
     return result;
 }
